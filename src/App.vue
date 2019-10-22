b7ba01576ef13a640367e20273b58f3cdc3ec89a
igorashs <igorashsirbu@gmail.com>
2019-10-22 16:05:32 +0300

    Add nav-bar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app>
-    <v-content></v-content>
+    <nav-bar />
+    <main>
+      <v-content></v-content>
+    </main>
   </v-app>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
+
 export default {
   name: 'App',
+  components: {
+    'nav-bar': NavBar,
+  },
   data: () => ({
     //
   }),
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <v-toolbar flat>
+      <v-toolbar-title class="display-1">
+        <span>Todo</span>
+        <span class="font-weight-bold">List</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed fab class="grey darken-3">
+        <v-icon x-large rigth>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
